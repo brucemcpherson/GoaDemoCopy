6e8025b773576a529a8ee7988ce6d07384961143
Bruce McPherson <bruce@mcpher.com>
2016-03-21 14:37:51 +0000

    updated by GasGit automation

---
+++ scripts/OnceOff.js
@@ -0,0 +1,36 @@
+function oneOffSetting() { 
+  
+  // used by all using this script
+  var propertyStore = PropertiesService.getScriptProperties();
+ 
+  // service account for cloud vision
+  cGoa.GoaApp.setPackage (propertyStore , 
+    cGoa.GoaApp.createServiceAccount (DriveApp , {
+      packageName: 'cloudvision_serviceaccount',
+      fileId:'0B92ExLh4POiZX1M2S3hMWXkzNXM',
+      scopes : cGoa.GoaApp.scopesGoogleExpand (['cloud-platform']),
+      service:'google_service'
+    }));
+  
+  // web account for cloud vision - taken from downloaded credentials
+  cGoa.GoaApp.setPackage (propertyStore , 
+    cGoa.GoaApp.createPackageFromFile (DriveApp , {
+      packageName: 'cloudvision',
+      fileId:'0B92ExLh4POiZUjIzbXFreTVPdjQ',
+      scopes : cGoa.GoaApp.scopesGoogleExpand (['cloud-platform','drive']),
+      service:'google',
+      apiKey:'AIzaxxxxh632MbPE'  // you can store arbirary properties in goa too.
+    }));
+  /*
+  // alternatively ... you can also set the credentials explicitly like this....
+  cGoa.GoaApp.setPackage (propertyStore , { 
+    clientId : "109xxxxxxxxxxusercontent.com",
+    clientSecret : "67xxxxxxxxxxxOxW",
+    scopes : cGoa.GoaApp.scopesGoogleExpand (['cloud-platform']),
+    service: 'google',
+    packageName: 'cloudvision',
+    apiKey:'AIzaSyAc728IODtd8s6t8WnpnC2JtrMdGvZKbdg'  // you can store arbirary properties in goa too.
+  });
+  */
+
+}
