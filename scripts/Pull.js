efe2b0f48376043e2dcda60f8bbeddd5ba872501
Bruce McPherson <bruce@mcpher.com>
2016-03-29 16:02:13 +0100

    updated by GasGit automation

--- scripts/Pull.js
+++ scripts/Pull.js
@@ -1,5 +1,5 @@
 var Pull = (function (ns) {
-
+  
   ns.dataFilters =  {
     scales: {
       "VERY_LIKELY":0.95, 
@@ -15,7 +15,9 @@ var Pull = (function (ns) {
         sorrow:"sorrowLikelihood",
         anger:"angerLikelihood",
         surprise:"surpriseLikelihood",
-        headwear:"headwearLikelihood"
+        headwear:"headwearLikelihood",
+        underExposed:"underExposedLikelihood",
+        blurred:"blurredLikelihood"       
       },
       scale:function (value) {
         var r = ns.dataFilters.scales[value];
@@ -38,68 +40,8 @@ var Pull = (function (ns) {
       }
     }
   };
+  
 
-  
-  /**
-  * do the general analysis
-  * @param {string} accessToken the accessToken
-  * @param {string} folderId the folder id with the images
-  * @param {string} type the type of feature
-  * @param {number} maxResults max number of results
-  * @param {string} [dataFilter] the name of the data filter to apply
-  * @param {boolean} [noCache] whether to suppress using cache 
-  * @param {boolean} 
-  * @return {object} the  result
-  */
-  ns.get = function (accessToken, folderId , type , maxResults, dataFilter,noCache) {
-    
-    
-    var result =  CloudVision.query (
-      accessToken, folderId, 
-      type , maxResults  , noCache
-    );
-    
-    
-    // select only the fields required
-    // and change their names
-    if (dataFilter) {
-      
-      // select and change the labels
-      var labels = ns.dataFilters[dataFilter].labels;
-      result.forEach(function(d) {
-        d.data = d.data.map(function(e) {
-          return Object.keys(labels).reduce (function (p,c) {
-            p[c] =  e[labels[c]];
-            return p;
-          },{});
-        });
-      });
-      
-      // any scaling
-      var scaling = ns.dataFilters[dataFilter].scale;
-      if (scaling) {
-        result.forEach(function(d) {
-          d.scales = Object.keys(labels).reduce(function (p,c) {
-            p[c] = d.data.reduce(function (pp,cc) {
-              return pp + scaling(cc[c]);
-            },0)/d.data.length;
-            return p;
-          },{}); 
-        });
-      }
-      
-    }
-    
-    // make the headers
-    result.forEach(function (d) {
-      if (d.data.length) {
-        d.headings = Object.keys(d.data[0]);
-      }
-    });
-    
-    return result;
-  };
-  
   /**
   * get data and change it around depending on the type of request
   * @param {string} accessToken the token to use
@@ -108,32 +50,99 @@ var Pull = (function (ns) {
   */
   ns.getDataFor = function (accessToken, package) {
     
+    var index = 0;
+    return package.providers.reduce(function (p,c) {
+      
+      var dataFilter =  package.dataFilters ? package.dataFilters[index] : null;
+
+      // different namespaces depending on provider
+      var r;
+      if ( c === "GOOGLE") {
+        r = CloudVision.query (
+          accessToken, package.folderId, 
+          package.feature , package.maxResponses  , package.noCache
+        );
+      }
+      else if (c==="MICROSOFT") {
+        r = MsEmotion.query (
+          accessToken , package.folderId, 
+          dataFilter , package.maxResponses  , package.noCache
+        );
+ 
+      }
+      else {
+        throw 'unkown provider ' + c;
+      }
+      
+
+
+      // select only the fields required
+      // and change their names      
+      if (dataFilter) {
+        // select and change the labels
+
+        var labels = ns.dataFilters[dataFilter].labels;
+
+        r.forEach(function(d) {
+          d.data = d.data.map(function(e) {
+            return Object.keys(labels).reduce (function (p,c) {
+
+              p[c] =  e[labels[c]];
+              return p;
+            },{});
+          });
+        });
+
+        // any scaling
+        var scaling = ns.dataFilters[dataFilter].scale;
+        if (scaling) {
+          r.forEach(function(d) {
+            d.scales = Object.keys(labels).reduce(function (p,c) {
+              p[c] = d.data.reduce(function (pp,cc) {
+                return pp + scaling(cc[c]);
+              },0)/d.data.length;
+              return p;
+            },{}); 
+          });
+        }
+        
+      }
+      
+      p[c]  = {
+        feature:package.feature,
+        queryResults:r,
+        headings: r.length  && r[0].data && r[0].data.length ? Object.keys(r[0].data[0]) : []
+      };
+      index++;
+      return p;
+    },{});
+    /**
     // do the analysis
     var data = ns.get ( 
-      accessToken, package.folderId, package.feature, 
-      package.maxResponses , package.dataFilter , package.noCache
+    accessToken, package.folderId, package.feature, 
+    package.maxResponses , package.dataFilter , package.noCache
     );
     
     // if its an emotion chart, going to compare against ms results
     if (package.dataFilter === "EMOTION")    {
-      var msData = MsEmotion.query (accessToken , package.folderId, 'MSEMOTION' , 1 , package.noCache ).map(function(d) {
-        return d.data;
-      });
-      return {
-        ms:{
-          data:msData,
-          headings:Object.keys(ns.dataFilters.MSEMOTION.labels)
-        },
-        google:data
-      }
+    var msData = MsEmotion.query (accessToken , package.folderId, 'MSEMOTION' , 1 , package.noCache ).map(function(d) {
+    return d.data;
+    });
+    return {
+    ms:{
+    data:msData,
+    headings:Object.keys(ns.dataFilters.MSEMOTION.labels)
+    },
+    google:data
+    }
     }
     else {
-      return data;
+    return data;
     }
-    
+    */
   };
   
-
+  
   /**
   * do the pets image analysis
   * @param {string} accessToken the accessToken
@@ -141,10 +150,15 @@ var Pull = (function (ns) {
   * @return {object} the pets result
   */
   ns.pets = function (accessToken,folderId) {
-    return ns.get (
-      accessToken, folderId || '0B92ExLh4POiZYzZZT3d0aU9VV1U', 
-      'LABEL_DETECTION' , 3 
-    );
+    return ns.getDataFor (accessToken, {
+      folderId : folderId || '0B92ExLh4POiZYzZZT3d0aU9VV1U',
+      feature : 'LABEL_DETECTION', 
+      maxResponses : 3,
+      providers:["GOOGLE"],
+      noCache:true
+    } 
+                         );
+    
   };
   
   /**
@@ -153,11 +167,14 @@ var Pull = (function (ns) {
   * @return {object} the pets result
   */
   ns.faces = function (accessToken,folderId) {
-    // some people images here
-    return ns.get (
-      accessToken, folderId || '0B92ExLh4POiZZDNKcVN0QTJJMGs', 
-      "FACE_DETECTION" , 1 , 'EMOTION'
-    );
+    return ns.getDataFor (accessToken, {
+      folderId : folderId || '0B92ExLh4POiZZDNKcVN0QTJJMGs',
+      feature : 'FACE_DETECTION', 
+      maxResponses : 3,
+      providers:["GOOGLE"]
+    } 
+                         );
+    
     
   };
   
