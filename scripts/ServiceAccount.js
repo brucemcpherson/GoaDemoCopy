e1ed4a89b852bc4fdff7f975d14e739abb2fb7a3
Bruce McPherson <bruce@mcpher.com>
2016-03-29 16:02:11 +0100

    updated by GasGit automation

--- scripts/ServiceAccount.js
+++ scripts/ServiceAccount.js
@@ -6,4 +6,35 @@ function usingServiceAccount () {
   logVision (Goth.getToken('cloudvision_serviceaccount',PropertiesService.getScriptProperties()));
 }
 
+/**
+ * log out the result of both tests
+ * @param {string} accessToken the access token
+ */
+function logVision (accessToken) {
 
+  // i have some animal images in this folder
+  // findout what they are of
+
+  var result = Pull.pets (accessToken);
+
+  Logger.log ( JSON.stringify(result.GOOGLE.queryResults.map(function(d) {
+    return {
+      name:d.file.fileName,
+      data:d.data,
+      scales:d.scales
+    };
+  })));
+  
+  // some faces
+  var result = Pull.faces (accessToken);
+  
+  // just summarize
+  Logger.log ( JSON.stringify(result.GOOGLE.queryResults.map(function(d) {
+    return {
+      name:d.file.fileName,
+      data:d.data,
+      scales:d.scales
+    };
+  })));
+  
+}
